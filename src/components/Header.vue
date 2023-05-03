03d79ea3f0cc90d89859aad5e24a92564b794da1
hervesson <hervesson@hotmail.com>
2023-05-03 18:03:30 -0300

    Exibindo o saldo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,6 +25,11 @@
                     </v-list-tile>
                 </v-list>
             </v-menu>
+            <v-layout align-center>
+                <span class="text-uppercase grey--text text--darken-2">
+                    Saldo: R$ {{ funds }}
+                </span>
+            </v-layout>
         </v-toolbar-items>
     </v-toolbar>
 </template>
@@ -32,7 +37,11 @@
 <script>
 
 export default {
-
+    computed: {
+        funds() {
+            return this.$store.getters.funds
+        }
+    }
 }
 </script>
 
